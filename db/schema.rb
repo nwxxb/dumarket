87e18393519292eaaf9476ea240e05b9a8da3da5
nwxxb <nawwabzia@gmail.com>
2025-12-17 16:11:04 +0700

    feat: add is_admin column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -50,6 +50,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_12_19_031539) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
