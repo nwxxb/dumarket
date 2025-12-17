092caa84c17689eadcbfe124270d608963d6ba83
nwxxb <nawwabzia@gmail.com>
2025-12-17 16:18:36 +0700

    fix/feat: init admin namespace & controller and split controller parent inheritance

--- config/routes.rb
+++ config/routes.rb
@@ -22,4 +22,8 @@ Rails.application.routes.draw do
 
   # this routes not get executed if there is \d{3}.html file in /public
   match "/:status", to: "errors#show", constraints: { status: /\d{3}/ }, via: :all
+
+  namespace :admin do
+    root "statics#home"
+  end
 end
