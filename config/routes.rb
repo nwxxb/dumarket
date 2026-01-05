f1dd9546dc254c956bd41269b7e110f429a74810
nwxxb <nawwabzia@gmail.com>
2026-01-05 11:13:28 +0700

    feat: init cart items controller and button to add cart item in products#show

--- config/routes.rb
+++ config/routes.rb
@@ -7,21 +7,14 @@ Rails.application.routes.draw do
   devise_scope :user do
     get "users/show", to: "users/registrations#show", as: "user_profile"
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/*
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
   # root "statics#home"
 
   root "products#index"
   resources :products, only: [ :index, :show ]
+
+  resources :cart_items, path: "cart/items", only: [ :index, :create, :update, :destroy ]
 
   # this routes not get executed if there is \d{3}.html file in /public
   match "/:status", to: "errors#show", constraints: { status: /\d{3}/ }, via: :all
@@ -30,4 +23,14 @@ Rails.application.routes.draw do
     root "statics#home"
     resources :products
   end
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  get "test", to: "statics#test"
+
+  # Render dynamic PWA files from app/views/pwa/*
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 end
