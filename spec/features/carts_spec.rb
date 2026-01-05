3c21a7c2130c8313f063181d94521623abef8183
nwxxb <nawwabzia@gmail.com>
2026-01-05 11:25:20 +0700

    test: init test for cart_items feature

---
+++ spec/features/carts_spec.rb
@@ -0,0 +1,229 @@
+require 'rails_helper'
+
+RSpec.feature "Cart Items", type: :feature, js: true do
+  context "authenticated user" do
+    it "can add products to cart" do
+      product1 = Fabricate(:product)
+      product2 = Fabricate(:product)
+      user = Fabricate(:user)
+      sign_in user
+
+      visit products_path
+      find_link(href: product_path(product1)).click
+      find(:test_id, 'add-to-cart').click
+      find(:test_id, 'add-to-cart').click
+
+      expect(page).to have_current_path(product_path(product1))
+
+      visit products_path
+      find_link(href: product_path(product2)).click
+      find(:test_id, 'add-to-cart').click
+
+      expect(page).to have_current_path(product_path(product2))
+
+      visit cart_items_path
+
+      expect(page).to have_selector(:test_id, 'cart-item-card', count: 2)
+      within(:test_id, 'cart-item-card', text: product1.name) do
+        expect(page).to have_selector(:test_id, 'cart-item-amount', text: "2")
+      end
+      within(:test_id, 'cart-item-card', text: product2.name) do
+        expect(page).to have_selector(:test_id, 'cart-item-amount', text: "1")
+      end
+    end
+
+    it "can update amount of a product in cart" do
+      product1 = Fabricate(:product)
+      user = Fabricate(:user)
+      sign_in user
+
+      visit products_path
+      find_link(href: product_path(product1)).click
+      find(:test_id, 'add-to-cart').click
+      find(:test_id, 'add-to-cart').click
+
+      visit cart_items_path
+
+      within(:test_id, 'cart-item-card', text: product1.name) do
+        find(:test_id, 'cart-item-increase-amount').click
+        find(:test_id, 'cart-item-increase-amount').click
+        find(:test_id, 'cart-item-decrease-amount').click
+      end
+      expect(page).to have_selector(:test_id, 'cart-item-card', count: 1)
+      within(:test_id, 'cart-item-card', text: product1.name) do
+        expect(page).to have_selector(:test_id, 'cart-item-amount', text: "3")
+      end
+    end
+
+    it "can remove a product in cart" do
+      product1 = Fabricate(:product)
+      user = Fabricate(:user)
+      sign_in user
+
+      visit products_path
+      find_link(href: product_path(product1)).click
+      find(:test_id, 'add-to-cart').click
+      find(:test_id, 'add-to-cart').click
+
+      visit cart_items_path
+
+      within(:test_id, 'cart-item-card', text: product1.name) do
+        find(:test_id, 'cart-item-delete').click
+      end
+      expect(page).to have_selector(:test_id, 'cart-item-card', count: 0)
+      expect(page).to have_content(/product..*remove..*cart/im)
+    end
+  end
+
+  context "guest user/unauthenticated user" do
+    it "can add products to cart" do
+      product1 = Fabricate(:product)
+      product2 = Fabricate(:product)
+      product3 = Fabricate(:product)
+      Fabricate(:cart_item, product: product3, session_id: SecureRandom.hex(16))
+
+      visit products_path
+      find_link(href: product_path(product1)).click
+      find(:test_id, 'add-to-cart').click
+      find(:test_id, 'add-to-cart').click
+
+      expect(page).to have_current_path(product_path(product1))
+
+      visit products_path
+      find_link(href: product_path(product2)).click
+      find(:test_id, 'add-to-cart').click
+
+      expect(page).to have_current_path(product_path(product2))
+
+      visit cart_items_path
+
+      expect(page).to have_selector(:test_id, 'cart-item-card', count: 2)
+      expect(page).not_to have_selector(:test_id, 'cart-item-card', text: product3.name)
+      within(:test_id, 'cart-item-card', text: product1.name) do
+        expect(page).to have_selector(:test_id, 'cart-item-amount', text: "2")
+      end
+      within(:test_id, 'cart-item-card', text: product2.name) do
+        expect(page).to have_selector(:test_id, 'cart-item-amount', text: "1")
+      end
+    end
+
+    it "can update amount of a product in cart" do
+      product1 = Fabricate(:product)
+
+      visit products_path
+      find_link(href: product_path(product1)).click
+      find(:test_id, 'add-to-cart').click
+      find(:test_id, 'add-to-cart').click
+
+      visit cart_items_path
+
+      within(:test_id, 'cart-item-card', text: product1.name) do
+        find(:test_id, 'cart-item-increase-amount').click
+        find(:test_id, 'cart-item-increase-amount').click
+        find(:test_id, 'cart-item-decrease-amount').click
+      end
+      expect(page).to have_selector(:test_id, 'cart-item-card', count: 1)
+      within(:test_id, 'cart-item-card', text: product1.name) do
+        expect(page).to have_selector(:test_id, 'cart-item-amount', text: "3")
+      end
+    end
+
+    it "can remove a product in cart" do
+      product1 = Fabricate(:product)
+
+      visit products_path
+      find_link(href: product_path(product1)).click
+      find(:test_id, 'add-to-cart').click
+      find(:test_id, 'add-to-cart').click
+
+      visit cart_items_path
+
+      within(:test_id, 'cart-item-card', text: product1.name) do
+        find(:test_id, 'cart-item-delete').click
+      end
+      expect(page).to have_selector(:test_id, 'cart-item-card', count: 0)
+      expect(page).to have_content(/product..*remove..*cart/im)
+    end
+
+    it "sign in will merge all cart item to existing cart" do
+      product1 = Fabricate(:product)
+      product2 = Fabricate(:product)
+      user = Fabricate(:user)
+      Fabricate(:cart_item, product: product1, user: user)
+
+      visit products_path
+      find_link(href: product_path(product1)).click
+      find(:test_id, 'add-to-cart').click
+
+      visit products_path
+      find_link(href: product_path(product2)).click
+      find(:test_id, 'add-to-cart').click
+
+      simulate_sign_in(user.email, user.password)
+
+      visit cart_items_path
+
+      expect(page).to have_selector(:test_id, 'cart-item-card', count: 2)
+      within(:test_id, 'cart-item-card', text: product1.name) do
+        expect(page).to have_selector(:test_id, 'cart-item-amount', text: "2")
+      end
+      within(:test_id, 'cart-item-card', text: product2.name) do
+        expect(page).to have_selector(:test_id, 'cart-item-amount', text: "1")
+      end
+
+      sign_out user
+      visit cart_items_path
+      expect(page).to have_selector(:test_id, 'cart-item-card', count: 0)
+    end
+
+    it "sign up will merge all cart item to existing cart" do
+      product1 = Fabricate(:product)
+      product2 = Fabricate(:product)
+      user = Fabricate.build(:user)
+
+      visit products_path
+      find_link(href: product_path(product1)).click
+      find(:test_id, 'add-to-cart').click
+      find(:test_id, 'add-to-cart').click
+
+      visit products_path
+      find_link(href: product_path(product2)).click
+      find(:test_id, 'add-to-cart').click
+
+      simulate_sign_up(user.email, user.password, user.password)
+
+      visit cart_items_path
+
+      expect(page).to have_selector(:test_id, 'cart-item-card', count: 2)
+      within(:test_id, 'cart-item-card', text: product1.name) do
+        expect(page).to have_selector(:test_id, 'cart-item-amount', text: "2")
+      end
+      within(:test_id, 'cart-item-card', text: product2.name) do
+        expect(page).to have_selector(:test_id, 'cart-item-amount', text: "1")
+      end
+
+      sign_out user
+      visit cart_items_path
+      expect(page).to have_selector(:test_id, 'cart-item-card', count: 0)
+    end
+  end
+
+  def simulate_sign_in(email, password)
+    visit new_user_session_path
+    within("form[action='#{user_session_path}']") do
+      fill_in :user_email, with: email
+      fill_in :user_password, with: password
+      find_button(type: 'submit').click
+    end
+  end
+
+  def simulate_sign_up(email, password, password_confirmation)
+    visit new_user_registration_path
+    within("form[action='#{user_registration_path}']") do
+      fill_in :user_email, with: email
+      fill_in :user_password, with: password
+      fill_in :user_password_confirmation, with: password_confirmation
+      find_button(type: 'submit').click
+    end
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#merge_cart_items_from_session" do
+    it "update session_id to nil and fill user_id if product not exist" do
+      user = Fabricate(:user)
+      product = Fabricate(:product)
+      session_id = SecureRandom.hex(16)
+      cart_item = Fabricate(
+        :cart_item, product: product, amount: 2,
+        user: nil, session_id: session_id
+      )
+
+      user.merge_cart_items_from_session(session_id)
+
+      expect(user.cart_items.length).to eq(1)
+      expect(user.cart_items.map(&:id)).to eq([ cart_item.id ])
+      cart_item.reload
+      expect(cart_item.user_id).to eq(user.id)
+      expect(cart_item.session_id).to eq(nil)
+      expect(user.cart_items.map(&:product_id)).to eq([ product.id ])
+      expect(user.cart_items.map(&:amount)).to eq([ 2 ])
+    end
+
+    it "increase amount if product already exist" do
+      user = Fabricate(:user)
+      product = Fabricate(:product)
+      session_id = SecureRandom.hex(16)
+      existing_cart_item = Fabricate(
+        :cart_item, product: product, amount: 1,
+        user: user, session_id: nil
+      )
+      existing_cart_item_2 = Fabricate(:cart_item, user: user, amount: 1)
+      cart_item = Fabricate(
+        :cart_item, product: product, amount: 2,
+        user: nil, session_id: session_id
+      )
+
+      user.merge_cart_items_from_session(session_id)
+
+      expect(user.cart_items.length).to eq(2)
+      expect(user.cart_items.map(&:id)).to match_array([ existing_cart_item.id, existing_cart_item_2.id ])
+      existing_cart_item.reload
+      expect(existing_cart_item.amount).to eq(3)
+      expect { cart_item.reload }.to raise_error(ActiveRecord::RecordNotFound)
+      expect(user.cart_items.map(&:product_id)).to include(product.id)
+      expect(user.cart_items.map(&:amount)).to match_array([ 3, 1 ])
+    end
+  end
 end
