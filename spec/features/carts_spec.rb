00ec61eee1064de9b43ab6d128db4bdac1b372be
nwxxb <nawwabzia@gmail.com>
2026-01-08 16:04:19 +0700

    test: move sign_in/sign_up steps as helpers for feature spec

--- spec/features/carts_spec.rb
+++ spec/features/carts_spec.rb
@@ -237,23 +237,4 @@ RSpec.feature "Cart Items", type: :feature, js: true do
       expect(page).to have_selector(:test_id, 'cart-item-card', count: 0)
     end
   end
-
-  def simulate_sign_in(email, password)
-    visit new_user_session_path
-    within("form[action='#{user_session_path}']") do
-      fill_in :user_email, with: email
-      fill_in :user_password, with: password
-      find_button(type: 'submit').click
-    end
-  end
-
-  def simulate_sign_up(email, password, password_confirmation)
-    visit new_user_registration_path
-    within("form[action='#{user_registration_path}']") do
-      fill_in :user_email, with: email
-      fill_in :user_password, with: password
-      fill_in :user_password_confirmation, with: password_confirmation
-      find_button(type: 'submit').click
-    end
-  end
 end
