5da5c8bb020dfa70fa9f338190f796f0c693c7d9
nwxxb <nawwabzia@gmail.com>
2026-01-22 09:43:15 +0700

    fix: remove statics#test route

--- config/routes.rb
+++ config/routes.rb
@@ -31,8 +31,6 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  get "test", to: "statics#test"
-
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
