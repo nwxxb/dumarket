34ff0aa201e03cddd8206bd37cf33360415a8ea6
nwxxb <nawwabzia@gmail.com>
2026-01-28 11:43:53 +0700

    feat & test: add basic stat/counter in admin/orders index page

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,10 @@
 class Admin::OrdersController < Admin::BaseController
   def index
     @orders = Order.all
+    @orders_count = @orders.size
+    @total_revenue = Money.from_cents(@orders.completed.sum(:total_amount_cents), "USD").format
+    @status_grouped_count = @orders.group(:status).count
+    @status_grouped_count.default = 0
   end
 
   def show
