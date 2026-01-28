d76779696bf557cb85672d7ced2e210dab348dc7
nwxxb <nawwabzia@gmail.com>
2026-01-28 11:43:53 +0700

    feat & test: add basic stat/counter in admin/orders index page

--- spec/features/admin/orders_spec.rb
+++ spec/features/admin/orders_spec.rb
@@ -19,6 +19,33 @@ RSpec.feature "Orders (admin)", type: :feature, js: true do
         expect(page).to have_link(href: admin_order_path(o))
       end
     end
+
+    it "admin can see the basic stats/counter" do
+      user = Fabricate(:user, is_admin: true)
+      Fabricate.times(1, :order, total_amount: 100, status: "pending")
+      Fabricate.times(2, :order, total_amount: 2, status: "cancelled")
+      Fabricate.times(3, :order, total_amount: 5, status: "completed")
+
+      sign_in user
+      visit admin_root_path
+      find_link(href: admin_orders_path).click
+
+      within(:test_id, "stat-box", text: /total..*order/im) do
+        expect(page).to have_content(6)
+      end
+      within(:test_id, "stat-box", text: /total..*revenue/im) do
+        expect(page).to have_content("$15.00")
+      end
+      within(:test_id, "stat-box", text: /pending/im) do
+        expect(page).to have_content(1)
+      end
+      within(:test_id, "stat-box", text: /cancelled/im) do
+        expect(page).to have_content(2)
+      end
+      within(:test_id, "stat-box", text: /completed/im) do
+        expect(page).to have_content(3)
+      end
+    end
   end
 
   describe "show" do
