6f20be5d2acfc597326a80b60bea2c6172a34b81
nwxxb <nawwabzia@gmail.com>
2026-02-08 16:03:48 +0700

    feat: init db seed to generate an admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+#
+
+User.create(email: "admin@dumarket.com", password: "admin123", is_admin: true)
