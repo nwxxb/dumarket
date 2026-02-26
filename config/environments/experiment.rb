b94476baf54a8fcfec351ed3a2a4af90ea3a1319
nwxxb <nawwabzia@gmail.com>
2026-02-26 13:23:28 +0700

    config/fix: add ability to add custom path to log file in experiment env

--- config/environments/experiment.rb
+++ config/environments/experiment.rb
@@ -6,7 +6,11 @@ Rails.application.configure do
 
   config.lograge.enabled = true
   config.lograge.keep_original_rails_log = false
-  config.lograge.logger = ActiveSupport::Logger.new "log/experiment_lograge.log"
+  config.lograge.logger = if ENV["RAILS_LOG_PATH"].present?
+    ActiveSupport::Logger.new ENV["RAILS_LOG_PATH"]
+  else
+    ActiveSupport::Logger.new "log/experiment_lograge.log"
+  end
   config.lograge.formatter = Lograge::Formatters::Json.new
 
   config.lograge.custom_options = lambda do |event|
