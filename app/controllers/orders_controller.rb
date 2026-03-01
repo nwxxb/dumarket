63166345c0d8e4b6db7e0ff87146d032902d423f
nwxxb <nawwabzia@gmail.com>
2026-03-01 17:35:21 +0700

    fix: add pagination to user pages
    - add pagination to `orders#index`
    - add pagination to `products#index`

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,7 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @orders = current_user.orders
+    @pagy, @orders = pagy(current_user.orders, limit: 6)
   end
 
   def new
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.kept
+    @pagy, @products = pagy(Product.kept)
   end
 
   def show
